31facbd848a2f5574c654de54f205d43d3abe69c
tprashan <prashantt9506@gmail.com>
2015-03-20 18:18:41 +0530

    refactor main of path

---
+++ FileReader.java
@@ -0,0 +1,34 @@
+package com.Path;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.*;
+
+
+public class FileReader {
+    public Map<String,List<String>> createDataBaseFromFileInput(String fileName){
+        Map<String,List<String>> cityDB = new HashMap<String,List<String>>();
+        File inFile = new File(fileName);
+        try{
+            FileInputStream fstream = new FileInputStream(inFile);
+            BufferedReader br = new BufferedReader(new InputStreamReader(fstream));
+            String strLine=br.readLine();
+            while ( strLine != null)   {
+                String[] cities = strLine.split(",");
+                if(cityDB.containsKey(cities[0])){
+                    List destination =  cityDB.get(cities[0]);
+                    destination.add(cities[1]);
+                }else{
+                    cityDB.put(cities[0], new ArrayList<String>(Arrays.asList(cities[1])));
+                }
+                strLine = br.readLine();
+            }
+
+        }catch (Exception e){
+            System.err.println("Error: " + e.getMessage());
+        }
+        return cityDB;
+    }
+}
